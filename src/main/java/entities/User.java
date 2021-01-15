59e58501dc2cd3531d5bae1d60426696ffbc259f
Rasmus Grønbek <groenbek@live.dk>
2021-01-15 14:03:11 +0100

    efter hack

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -4,12 +4,14 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Basic;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -35,6 +37,10 @@ public class User implements Serializable {
         @JoinColumn(name = "role_name", referencedColumnName = "role_name")})
     @ManyToMany
     private List<Role> roleList = new ArrayList<>();
+    
+    @OneToOne(cascade = CascadeType.PERSIST)
+    private Pants pants;
+        
 
     public List<String> getRolesAsStrings() {
         if (roleList.isEmpty()) {
@@ -87,6 +93,17 @@ public class User implements Serializable {
 
     public void addRole(Role userRole) {
         roleList.add(userRole);
+    }
+       public void addPants(Pants pants) {
+        this.pants = pants;
+    }
+
+    public Pants getPants() {
+        return pants;
+    }
+
+    public void setPants(Pants pants) {
+        this.pants = pants;
     }
 
 }
